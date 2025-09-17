825a9ab7c38700e7e787eb5916d00d40e8ed6215
vishalsparmar <vishalsparmar@yahoo.com>
2025-09-17 20:19:50 +0100

    Create createR53.tf

---
+++ Route53/createR53.tf
@@ -0,0 +1,58 @@
+resource "aws_route53_zone" "pet" {
+  name = "pet.example.com"
+}
+
+# IAM Role for ExternalDNS with OIDC trust (EKS IRSA)
+data "aws_iam_openid_connect_provider" "eks" {
+  arn = "arn:aws:iam::<account-id>:oidc-provider/oidc.eks.<region>.amazonaws.com/id/<eks-oidc-id>"
+}
+
+resource "aws_iam_role" "externaldns" {
+  name = "externaldns-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = data.aws_iam_openid_connect_provider.eks.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "oidc.eks.<region>.amazonaws.com/id/<eks-oidc-id>:sub" = "system:serviceaccount:external-dns:external-dns"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "externaldns" {
+  name        = "externaldns-policy"
+  description = "Allow ExternalDNS to manage Route53 records"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "route53:ChangeResourceRecordSets",
+          "route53:ListResourceRecordSets"
+        ],
+        Resource = "arn:aws:route53:::hostedzone/${aws_route53_zone.pet.zone_id}"
+      },
+      {
+        Effect   = "Allow",
+        Action   = ["route53:ListHostedZones"],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "externaldns_attach" {
+  role       = aws_iam_role.externaldns.name
+  policy_arn = aws_iam_policy.externaldns.arn
+}
